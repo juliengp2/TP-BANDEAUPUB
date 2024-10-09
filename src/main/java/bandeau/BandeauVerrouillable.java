b2bd348ba7fb1b4ee3ed3326a09b9be3db15ee07
juliengp <julien.garciaperez@gmail.com>
2024-10-09 09:51:11 +0200

    BandeauPub fini

--- src/main/java/bandeau/BandeauVerrouillable.java
+++ src/main/java/bandeau/BandeauVerrouillable.java
@@ -1,8 +1,38 @@
 package bandeau;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BandeauVerrouillable extends Bandeau implements Runnable {
+    private final Lock lock = new ReentrantLock();
+    private boolean isPlaying = false;
+    private final Scenario scenario;
+
+    public BandeauVerrouillable(Scenario scenario) {
+        this.scenario = scenario;
+    }
+
     @Override
     public void run() {
-        
+        lock.lock();
+        try {
+            if (isPlaying) {
+                return;
+            }
+            isPlaying = true;
+        } finally {
+            lock.unlock();
+        }
+
+        try {
+            scenario.playOn(this);
+        } finally {
+            lock.lock();
+            try {
+                isPlaying = false;
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 }
--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -1,6 +1,9 @@
 package bandeau;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Classe utilitaire pour représenter la classe-association UML
@@ -22,6 +25,9 @@ class ScenarioElement {
 public class Scenario {
 
     private final List<ScenarioElement> myElements = new LinkedList<>();
+    private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private final Lock readLock = rwLock.readLock();
+    private final Lock writeLock = rwLock.writeLock();
 
     /**
      * Ajouter un effect au scenario.
@@ -30,7 +36,12 @@ public class Scenario {
      * @param repeats le nombre de répétitions pour cet effet
      */
     public void addEffect(Effect e, int repeats) {
-        myElements.add(new ScenarioElement(e, repeats));
+        writeLock.lock();
+        try {
+            myElements.add(new ScenarioElement(e, repeats));
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     /**
@@ -39,10 +50,15 @@ public class Scenario {
      * @param b le bandeau ou s'afficher.
      */
     public void playOn(Bandeau b) {
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(b);
+        readLock.lock();
+        try {
+            for (ScenarioElement element : myElements) {
+                for (int repeats = 0; repeats < element.repeats; repeats++) {
+                    element.effect.playOn(b);
+                }
             }
+        } finally {
+            readLock.unlock();
         }
     }
 }
